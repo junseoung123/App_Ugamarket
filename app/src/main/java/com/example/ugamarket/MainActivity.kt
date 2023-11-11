58772c911547046ffc0ebb25c631680dbe362b7d
ahntd <gaejugi6831@gmail.com>
2023-11-11 17:19:26 +0900

    ahntd 주석달기 테스트

--- app/src/main/java/com/example/ugamarket/MainActivity.kt
+++ app/src/main/java/com/example/ugamarket/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //ahntd 주석달기 테스트
     }
 }
