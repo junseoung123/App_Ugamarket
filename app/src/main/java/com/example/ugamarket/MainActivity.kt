49e8dad35d2a1ba5a8bf14e07f590bebc124fd38
kimchiman123 <leo010516@naver.com>
2023-11-12 13:59:01 +0900

    ㅎㅇㅎㅇ

--- app/src/main/java/com/example/ugamarket/MainActivity.kt
+++ app/src/main/java/com/example/ugamarket/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //ahntd 주석달기 테스트
+        //ㅎㅇ
     }
 }
